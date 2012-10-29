d352afa378642722b614b8451b839886a8d9dee6
Jean-Baptiste Minvielle <jb@procheo.fr>
2012-10-29 17:36:09 +0100

    structure HTML de challenge en vue d'une intégration PHP

---
+++ challenge.php
@@ -0,0 +1,32 @@
+<html xmlns="http://www.w3.org/1999/xhtml"
+xmlns:og="http://ogp.me/ns#"
+xmlns:fb="http://www.facebook.com/2008/fbml">
+
+<head>
+	<head prefix="og: http://ogp.me/ns# challengemehetic: 
+                  http://ogp.me/ns/apps/challengemehetic#">
+
+	<!-- Identifiant de l'application -->
+	<meta property="fb:app_id" content="267328883388489" />
+	<!-- Propriétés de l'objet --> 
+	<meta property="og:type" content="challengemehetic:challenge" /> 
+	<meta property="og:title" content="Gobage de famby !" /> 
+	<meta property="og:description" content="Ce défi consiste à gober un flamby." /> 
+	<meta property="og:image" content="http://www.transeet.fr/wp-content/uploads/2012/05/radiolondres-pays-bas-hongrie-hollande-vainqueur.gif" /> 
+	<meta property="og:url" content="http://jeanbaptisteminvielle.fr/challengeme/tests/openGraph.html" />
+
+	<title>Challenge "Gober un Famby"</title>
+</head>
+
+
+
+<body>
+
+<a href="" title="lancer ce défi pour vous-même">Accepter ce défi</a href="">
+<a href="" title="lancer ce défi pour un ami Facebook">Envoyer ce défi à un ami</a>
+<a href="" title="indiquez que vous avez réalisé ce défi">J'ai réalisé ce défi !</a>
+
+
+
+</body>
+</html>
