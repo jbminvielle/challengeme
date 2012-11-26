534a01686e389a8c72904b02d69af20cc25f4ad5
Anne-Cécile SCHMITT <annececile.schmitt@gmail.com>
2012-11-26 19:38:47 +0100

    Intégration de la liste des défis

---
+++ integration/liste_challenge.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+
+	<?php require('header.php'); ?>
+	<span class="separator">&nbsp;</span>
+	<section>
+		<header id="defi-semaine-head">
+			<h1>Le défi de la semaine</h1>
+		</header>
+		<img src="" alt="Psy_Gangnam_Style" title="Psy_Gangnam_Style">
+		<h2>"Dansez le Gangnam Style en public"</h2>
+		<p>
+			<span>Votre objectif :</span>
+				Suspendisse commodo, metus nec tristique facilisis, 
+				justo leo tristique dolor, nec mattis dolor justo quis felis. 
+				Pellentesque eleifend adipiscing orci sit amet suscipit.
+		</p>
+		<a href="">Participer</a>
+		<a href="">Voir les vidéos</a>
+		<div id="information_defi">
+			<ul>
+				<li>Temps restant :</li>
+				<li>une date</li>
+				<li>Participants :</li>
+				<li> nbr participants</li>
+				<li>Nombre de votes :</li>
+				<li>nbr votes</li>
+			</ul>
+		</div>
+		<div id="podium">
+			<ul>
+				<li>Le podium actuel :</li>
+				<li>1er</li>
+				<li>Nom 1</li>
+				<li>30 likes</li>
+				<li>2ème</li>
+				<li>Nom 2</li>
+				<li>20 likes</li>
+				<li>3ème</li>
+				<li>Nom 3</li>
+				<li>10 likes</li>
+			</ul>
+		</div>
+	</section>
+	<section>
+		<header>
+			<h1>Les défis</h1>
+			<div id="bandeau-participer">
+				<a href="#">Soumettre un défi</a>
+				<a href="#">Défier un ami +fb</a>
+			</div>
+			<div id="bandeau-participer">
+				<a href="#">Afficher tous les défis</a>
+				<a href="#">Trouver un défi</a>
+				<select>
+					<option value="0">Choisir un défi</option>
+					<option value="1">Défi 1</option>
+					<option value="2">Défi 2</option>
+					<option value="3">Défi 3</option>
+				</select>
+			</div>
+		</header>
+		<div>
+			<div class="défis_description">
+				<img src="" alt="" title="">
+				<h2>Gobage de Flamby</h2>
+				<p>
+					Suspendisse commodo, metus nec tristique facilisis, justo leo . 
+					Suspendisse commodo...
+				</p>
+				<a href="">Participer</a>
+			</div>
+			<div class="top_video">
+				<h3>Le top de ce défi</h3>
+				<img src="" alt="" title="">
+				<p>Nom 1</p>
+			</div>
+		</div>
+		<div>
+			<div class="défis_description">
+				<img src="" alt="" title="">
+				<h2>Gobage de Flamby</h2>
+				<p>
+					Suspendisse commodo, metus nec tristique facilisis, justo leo . 
+					Suspendisse commodo...
+				</p>
+				<a href="">Participer</a>
+			</div>
+			<div class="top_video">
+				<h3>Le top de ce défi</h3>
+				<img src="" alt="" title="">
+				<p>Nom 1</p>
+			</div>
+		</div>
+		<div>
+			<div class="défis_description">
+				<img src="" alt="" title="">
+				<h2>Gobage de Flamby</h2>
+				<p>
+					Suspendisse commodo, metus nec tristique facilisis, justo leo . 
+					Suspendisse commodo...
+				</p>
+				<a href="">Participer</a>
+			</div>
+			<div class="top_video">
+				<h3>Le top de ce défi</h3>
+				<img src="" alt="" title="">
+				<p>Nom 1</p>
+			</div>
+		</div>
+	</section>
+		<?php require('footer.php'); ?>
+	</div>
+	
+</body>
+</html>
