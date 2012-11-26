45a7ad6c20a4b5576a5212c667fab1d14e4e9087
Anne-Cécile SCHMITT <annececile.schmitt@gmail.com>
2012-11-26 23:17:52 +0100

    Suite de l'inté de la liste de défis

--- integration/liste_challenge.php
+++ integration/liste_challenge.php
@@ -7,55 +7,59 @@
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 
+	<link rel="stylesheet" href="css/style_temp.css">
 	<?php require('header.php'); ?>
 	<span class="separator">&nbsp;</span>
-	<section>
+	<section id="defi_de_la_semaine">
 		<header id="defi-semaine-head">
 			<h1>Le défi de la semaine</h1>
 		</header>
-		<img src="" alt="Psy_Gangnam_Style" title="Psy_Gangnam_Style">
-		<h2>"Dansez le Gangnam Style en public"</h2>
-		<p>
-			<span>Votre objectif :</span>
-				Suspendisse commodo, metus nec tristique facilisis, 
-				justo leo tristique dolor, nec mattis dolor justo quis felis. 
-				Pellentesque eleifend adipiscing orci sit amet suscipit.
-		</p>
-		<a href="">Participer</a>
-		<a href="">Voir les vidéos</a>
-		<div id="information_defi">
-			<ul>
-				<li>Temps restant :</li>
-				<li>une date</li>
-				<li>Participants :</li>
-				<li> nbr participants</li>
-				<li>Nombre de votes :</li>
-				<li>nbr votes</li>
-			</ul>
-		</div>
-		<div id="podium">
-			<ul>
-				<li>Le podium actuel :</li>
-				<li>1er</li>
-				<li>Nom 1</li>
-				<li>30 likes</li>
-				<li>2ème</li>
-				<li>Nom 2</li>
-				<li>20 likes</li>
-				<li>3ème</li>
-				<li>Nom 3</li>
-				<li>10 likes</li>
-			</ul>
+		<div id="absolute">
+			<img src="" alt="Psy_Gangnam_Style" title="Psy_Gangnam_Style" width="376px" height="210px">
+			<h2>"Dansez le Gangnam Style en public"</h2>
+			<p>
+				<span>Votre objectif :</br></span>
+					Suspendisse commodo, metus nec tristique facilisis, 
+					justo leo tristique dolor, nec mattis dolor justo quis felis. 
+					Pellentesque eleifend adipiscing orci sit amet suscipit.
+			</p>
+			<a href="" class="call_action">Participer</a>
+			<a href="" class="call_action">Voir les vidéos</a>
+			<div id="information_defi">
+				<ul>
+					<li>Temps restant :</li>
+					<li class="orange">une date</li>
+					<li>Participants :</li>
+					<li class="orange"> nbr</li>
+					<li>Votes :</li>
+					<li class="orange">nbr votes</li>
+				</ul>
+			</div>
+			<div id="podium">
+				<ul>
+					<li id="titre">Le podium actuel :</li>
+					<li class="position">1er</li>
+					<li class="nom"><a href="">Nom 1</a></li>
+					<li class="likes">30 likes</li>
+					<li class="position">2ème</li>
+					<li class="nom"><a href="">Nom 2</a></li>
+					<li class="likes">20 likes</li>
+					<li class="position">3ème</li>
+					<li class="nom"><a href="">Nom 3</a></li>
+					<li class="likes">10 likes</li>
+				</ul>
+			</div>
 		</div>
 	</section>
-	<section>
+	<section id="autre_defi">
 		<header>
 			<h1>Les défis</h1>
+			<p id="ligne_orange"></p>
 			<div id="bandeau-participer">
 				<a href="#">Soumettre un défi</a>
 				<a href="#">Défier un ami +fb</a>
 			</div>
-			<div id="bandeau-participer">
+			<div id="bandeau-defi">
 				<a href="#">Afficher tous les défis</a>
 				<a href="#">Trouver un défi</a>
 				<select>
@@ -66,10 +70,10 @@
 				</select>
 			</div>
 		</header>
-		<div>
-			<div class="défis_description">
-				<img src="" alt="" title="">
-				<h2>Gobage de Flamby</h2>
+		<div id="liste_des_defis">
+			<div class="defis_description">
+				<img src="" alt="" title="" width="194px" height="109px">
+				<h2>"Gobage de Flamby"</h2>
 				<p>
 					Suspendisse commodo, metus nec tristique facilisis, justo leo . 
 					Suspendisse commodo...
@@ -78,14 +82,30 @@
 			</div>
 			<div class="top_video">
 				<h3>Le top de ce défi</h3>
-				<img src="" alt="" title="">
-				<p>Nom 1</p>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 1</p>
+					</a>
+				</div>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 2</p>
+					</a>
+				</div>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 3</p>
+					</a>
+				</div>
 			</div>
 		</div>
-		<div>
-			<div class="défis_description">
-				<img src="" alt="" title="">
-				<h2>Gobage de Flamby</h2>
+		<div id="liste_des_defis">
+			<div class="defis_description">
+				<img src="" alt="" title="" width="194px" height="109px">
+				<h2>"Gobage de Flamby"</h2>
 				<p>
 					Suspendisse commodo, metus nec tristique facilisis, justo leo . 
 					Suspendisse commodo...
@@ -94,14 +114,30 @@
 			</div>
 			<div class="top_video">
 				<h3>Le top de ce défi</h3>
-				<img src="" alt="" title="">
-				<p>Nom 1</p>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 1</p>
+					</a>
+				</div>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 2</p>
+					</a>
+				</div>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 3</p>
+					</a>
+				</div>
 			</div>
 		</div>
-		<div>
-			<div class="défis_description">
-				<img src="" alt="" title="">
-				<h2>Gobage de Flamby</h2>
+		<div id="liste_des_defis">
+			<div class="defis_description">
+				<img src="" alt="" title="" width="194px" height="109px">
+				<h2>"Gobage de Flamby"</h2>
 				<p>
 					Suspendisse commodo, metus nec tristique facilisis, justo leo . 
 					Suspendisse commodo...
@@ -110,8 +146,24 @@
 			</div>
 			<div class="top_video">
 				<h3>Le top de ce défi</h3>
-				<img src="" alt="" title="">
-				<p>Nom 1</p>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 1</p>
+					</a>
+				</div>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 2</p>
+					</a>
+				</div>
+				<div>
+					<a href="">
+						<img src="" alt="" title="" width="104px" height="58px">
+						<p>Nom 3</p>
+					</a>
+				</div>
 			</div>
 		</div>
 	</section>
