921f96260b6aa44bb586b60332db8a8c49c5f555
Constance Souville <souville.constance@gmail.com>
2012-12-05 13:33:19 +0100

    reparation style_temp.css

--- integration/challenge.php
+++ integration/challenge.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<!doctype html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title></title>
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-
-	<!-- Identifiant de l'application opengraph -->
-	<meta property="fb:app_id" content="267328883388489" />
-	<!-- Propriétés de l'objet --> 
-	<meta property="og:type" content="challengemehetic:challenge" /> 
-	<meta property="og:title" content="Gobage de famby !" /> 
-	<meta property="og:description" content="Ce défi consiste à gober un flamby lolilol" /> 
-	<meta property="og:image" content="medias/challenges_images/gobage_flamby.png" /> 
-	<meta property="og:url" content="challenge.php?gobage-famby" />
-
-	<?php include('header.php'); ?>
-	<!-- Page pour le défi "gobage de flamby" -->
-
-	<h1>Gobage de famby</h1>
-
-		<?php include('footer.php'); ?>
-	</div>
-	
-</body>
-=======
 <!doctype html>
 <html lang="fr">
 <head>
@@ -105,5 +76,4 @@
 	</div>
 	
 </body>
->>>>>>> 7aacef9dc2632cb7655be09467a9389d4c2cadde
 </html>
